223ee2a5cb78e90c544dc354c66f0995ccf7d015
Benjamin Duppe <benjamin.duppe@dfki.de>
2023-06-21 11:15:43 +0200

    ClearML integration

--- startClearMLClient.py
+++ startClearMLClient.py
@@ -1,9 +1,9 @@
 from clearml import Task
-import client
-
-client_number = 0
 
 task = Task.init(project_name='FL_MRT', task_name=f'Client {client_number}')
 task.execute_remotely(queue_name='default', clone=False, exit_process=True)
+
+client_number = 0
+import client
 
 client.start_client("127.0.0.1")
