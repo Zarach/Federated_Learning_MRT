3b77e51d0cb34a7d54fe09731565a3a37f7db0a0
Benjamin Duppe <benjamin.duppe@dfki.de>
2023-06-21 11:18:46 +0200

    ClearML integration

--- startClearMLClient.py
+++ startClearMLClient.py
@@ -1,4 +1,4 @@
-from clearml import Task
+from clearml import Task, Dataset
 
 client_number = 0
 
@@ -7,5 +7,9 @@ task.execute_remotely(queue_name='default', clone=False, exit_process=True)
 
 
 import client
+
+# get local copy of DataBases
+dataset_databases = Dataset.get(dataset_project='FL_MRT', dataset_name='MRT_Raw_Data')
+dataset_path_databases = dataset_databases.get_mutable_local_copy("raw_data/", True)
 
 client.start_client("127.0.0.1")
